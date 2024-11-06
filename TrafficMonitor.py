1973d36fe083b5b7abce3dda673ac4af0bce3d57
larilaier <107729339+larilaier@users.noreply.github.com>
2024-11-06 15:53:23 -0300

    Update TrafficMonitor.py

--- TrafficMonitor.py
+++ TrafficMonitor.py
@@ -1,29 +1,19 @@
-import socket
+from scapy.all import sniff
 
 class TrafficMonitor:
-    def __init__(self, interface="eth0"):
+    def __init__(self, interface="Ethernet"):
         self.interface = interface
 
     def start_sniffer(self):
-        sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-        sock.bind((self.interface, 0))
-        while True:
-            raw_data, addr = sock.recvfrom(65536)
-            self.process_packet(raw_data)
+        print(f"Iniciando captura de pacotes na interface {self.interface}")
+        sniff(iface=self.interface, prn=self.process_packet, store=False)
 
-    def process_packet(self, data):
-        # Aqui você pode implementar a detecção de pacotes HTTP e DNS
-        if self.is_http_packet(data):
-            print("Pacote HTTP detectado")
-            # Extraia e salve os dados HTTP
-        elif self.is_dns_packet(data):
-            print("Pacote DNS detectado")
-            # Extraia e salve os dados DNS
-
-    def is_http_packet(self, data):
-        return b"HTTP" in data
-
-    def is_dns_packet(self, data):
-        return data[23] == 17 and data[36:38] == b'\x00\x35'  # Porta 53 para DNS
-
-#capturar e analisar o tráfego da rede
+    def process_packet(self, packet):
+        if packet.haslayer('IP'):
+            ip_src = packet['IP'].src
+            ip_dst = packet['IP'].dst
+            print(f"Pacote de IP: {ip_src} para {ip_dst}")
+            if packet.haslayer('TCP') and packet['TCP'].dport == 80:
+                print("Pacote HTTP detectado")
+            elif packet.haslayer('UDP') and packet['UDP'].dport == 53:
+                print("Pacote DNS detectado")
