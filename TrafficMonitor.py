45df7320a694cfd576af8b6a77461e8a4e1ed8f1
larilaier <107729339+larilaier@users.noreply.github.com>
2024-11-11 15:37:37 -0300

    Update TrafficMonitor.py

--- TrafficMonitor.py
+++ TrafficMonitor.py
@@ -1,4 +1,4 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP, TCP, UDP, DNS
 
 class TrafficMonitor:
     def __init__(self, interface="Ethernet"):
@@ -9,11 +9,17 @@ class TrafficMonitor:
         sniff(iface=self.interface, prn=self.process_packet, store=False)
 
     def process_packet(self, packet):
-        if packet.haslayer('IP'):
-            ip_src = packet['IP'].src
-            ip_dst = packet['IP'].dst
+        if packet.haslayer(IP):
+            ip_src = packet[IP].src
+            ip_dst = packet[IP].dst
             print(f"Pacote de IP: {ip_src} para {ip_dst}")
-            if packet.haslayer('TCP') and packet['TCP'].dport == 80:
+            
+            if packet.haslayer(TCP) and packet[TCP].dport == 80:
                 print("Pacote HTTP detectado")
-            elif packet.haslayer('UDP') and packet['UDP'].dport == 53:
+                if packet.haslayer('Raw'):
+                    print("Dados HTTP:", packet['Raw'].load.decode(errors='ignore'))
+            
+            elif packet.haslayer(UDP) and packet[UDP].dport == 53 and packet.haslayer(DNS):
                 print("Pacote DNS detectado")
+                if packet[DNS].qd:
+                    print(f"Consulta DNS: {packet[DNS].qd.qname.decode('utf-8')}")
